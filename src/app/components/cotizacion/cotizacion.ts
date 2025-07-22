788ed64cc3e6963cd040cb0615b169c347c702df
IDGS-803-22000965 <79228@alumnos.utleon.edu.mx>
2025-07-21 23:48:31 -0600

    Primer Version

---
+++ src/app/components/cotizacion/cotizacion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cotizacion',
+  imports: [],
+  templateUrl: './cotizacion.html',
+  styleUrl: './cotizacion.scss'
+})
+export class Cotizacion {
+
+}
